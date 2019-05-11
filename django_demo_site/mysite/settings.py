3e92895e373afb1f89cd23d7682a6d3ae7e29553
kylezou <moringingkyle@gmail.com>
2019-05-11 09:58:15 +0800

    defining STATIC_ROOT that fits to different os and location

--- django_demo_site/mysite/settings.py
+++ django_demo_site/mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_DIR)
@@ -123,7 +124,7 @@ LOGIN_REDIRECT_URL = '/post'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = "/var/www/cms_site/django_demo_site/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, os.path.pardir, "static")
 STATIC_URL = '/static/'
 
 CKEDITOR_CONFIGS = {
